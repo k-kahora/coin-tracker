dc33e4f3f81c76569025d67b0b52f2eed2b14703
k-kahora <mango22mjk@gmail.com>
2024-02-17 20:41:32 +0000

    dimensions api call done

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template
-from flask import Flask, jsonify
+from flask import Flask, render_template, jsonify, request
 import requests
 
 app = Flask(__name__)
@@ -19,7 +18,29 @@ def fetch_coins():
     else:
         return jsonify({"error": "Failed to fetch data from CoinGecko"}), response.status_code
 
-@app.route('/dimensions')
+
+@app.route('/dimensions', methods=['GET'])
+def dimension():
+    query_param = request.args.get('query', None)
+
+    # Check if the query parameter is provided
+    if not query_param:
+        return jsonify({"error": "Missing query parameter"}), 400
+    url = f"https://api.coingecko.com/api/v3/coins/{query_param}?tickers=false&market_data=true&community_data=true&developer_data=false&sparkline=false"
+    headers = {'accept': 'application/json'}
+    
+    # Make the API call
+    try:
+        response = requests.get(url, headers=headers)
+        # Assuming the API returns JSON
+        data = response.json()
+        
+        # Return the data from the API call
+        return jsonify(data)
+    except requests.RequestException as e:
+        # Handle connection errors
+        return jsonify({"error": "Failed to make API call", "details": str(e)}), 500
+
 @app.route('/main-page')
 def main_site():
     return render_template('graph.html')
