5b5fe6b67773f502026fd526ade0fceb396e7729
k-kahora <mango22mjk@gmail.com>
2024-02-17 17:54:08 +0000

    App is up and running with all the coins

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Welcome to the CoinGecko Data Fetcher!'
+
+@app.route('/fetch-coins')
+def fetch_coins():
+    url = 'https://api.coingecko.com/api/v3/coins/list?include_platform=true'
+    headers = {'accept': 'application/json'}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        return jsonify(data)
+    else:
+        return jsonify({"error": "Failed to fetch data from CoinGecko"}), response.status_code
+
+if __name__ == '__main__':
+    app.run(debug=True)
