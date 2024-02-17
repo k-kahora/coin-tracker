c298d0e5b088c3c4b1d54b523b113d5711c761b9
k-kahora <mango22mjk@gmail.com>
2024-02-17 20:02:12 +0000

    Plan is in place

--- static/js/dd3.js
+++ static/js/dd3.js
@@ -1,6 +1,23 @@
+// set the dimensions and margins of the graph
+var margin = {top: 30, right: 10, bottom: 10, left: 0},
+  width = 500 - margin.left - margin.right,
+  height = 400 - margin.top - margin.bottom;
+
+
+// append the svg object to the body of the page
+var svg = d3.select("#my_dataviz")
+.append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+.append("g")
+  .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")");
+
+
 d3.json('/fetch-coins').then(function(data) {
     // Use D3 to visualize the data
     console.log(data); // For debugging purposes
+    console.log(data[100].id)
     // Example: Create a simple visualization based on the data
     // This is where you'd use D3.js methods to bind data to DOM elements, create SVGs, etc.
 });
