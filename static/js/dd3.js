cc86401cd30bd210a633ecb140c96b1f5e2e0c4f
k-kahora <mango22mjk@gmail.com>
2024-02-18 16:03:39 +0000

    Filtering is compete

--- static/js/dd3.js
+++ static/js/dd3.js
@@ -2,6 +2,8 @@
 var margin = {top: 30, right: 10, bottom: 10, left: 0},
   width = 1000 - margin.left - margin.right,
   height = 400 - margin.top - margin.bottom;
+let globalSelections = {};
+
 
 // append the svg object to the body of the page
 // var wow = d3.keys()
@@ -18,16 +20,21 @@ d3.json('/clean-data').then(function(data) {
     // Use D3 to visualize the data
     // Example: Create a simple visualization based on the data
     // This is where you'd use D3.js methods to bind data to DOM elements, create SVGs, etc.
-
     var columns_col = ["name", "logo"].concat(d3.keys(data[0]).filter(key => !["name", "logo"].includes(key)));
     dimensions = d3.keys(data[0]).filter(function(d) { return d != "name" && d != "logo" })
+dimensions.forEach(dim => globalSelections[dim] = null); // Assuming 'dimensions' is your array of axes names
     var y = {}
+    var x
+    
+     function updateGraph(new_data) {
+
     for (i in dimensions) {
 	name = dimensions[i]
 	y[name] = d3.scaleLinear()
-	    .domain( d3.extent(data, function(d) { return parseFloat(d[name]); }) ) // TODO make sure the numbers are returend in the valide format
+	    .domain( d3.extent(new_data, function(d) { return parseFloat(d[name]); }) ) // TODO make sure the numbers are returend in the valide format
 	    .range([height, 0])
     }
+
     x = d3.scalePoint()
 	.range([0, width])
 	.padding(1)
@@ -35,6 +42,7 @@ d3.json('/clean-data').then(function(data) {
  function path(d) {
       return d3.line()(dimensions.map(function(p) { return [x(p), y[p](d[p])]; }));
   }
+
   svg
     .selectAll("myPath")
     .data(data)
@@ -57,6 +65,9 @@ var axis = svg.selectAll("myAxis")
       .attr("y", -9)
       .text(function(d) { return d; })
       .style("fill", "black")
+     }
+    updateGraph(data)
+
     
 dimensions.forEach(function(dimension) {
         var brush = d3.brushY()
@@ -79,14 +90,30 @@ dimensions.forEach(function(dimension) {
             .attr("transform", "translate(" + x(dimension) + ",0)")
             .call(brush);
 
-        function brushed(event) {
-            var selection = event.selection;
-	    if (selection == null) return;
-	    var [y0, y1] = selection.map(y[dimension].invert, y[dimension]);
-	    var filteredData = data.filter(function(d) {
+function brushed(event) {
+    // Determine the selection extent for the current brush
+    var selection = event.selection;
+    if (selection) {
+        var [y0, y1] = selection.map(y[dimension].invert, y[dimension]);
+        globalSelections[dimension] = [y0, y1]; // Update the selection for the current axis
+    } else {
+        globalSelections[dimension] = null; // Clear the selection if the brush is cleared
+    }
 
-		return parseFloat(d[dimension]) >= y1 && parseFloat(d[dimension]) <= y0;
-	    });
+    // Filter data based on global selections
+    var filteredData = data.filter(d => {
+        return dimensions.every(dim => {
+            // If no selection for an axis, or the item is within the selection, return true
+            if (!globalSelections[dim]) return true;
+            var val = d[dim];
+            var [max, min] = globalSelections[dim];
+	    console.log([min,max])
+            return val >= min && val <= max;
+        });
+    });
+
+    // Now update your visualization with filteredData
+    // For example, you might call a function to update the table or other visual elements
 svg.selectAll("path")
         .style("opacity", 0.1); // Dim all paths
     svg.selectAll("path")
